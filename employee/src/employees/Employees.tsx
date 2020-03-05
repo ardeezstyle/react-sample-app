93714333fb2aa9e3169d96e1cd6b90535a11eb9e
Raman Mandal <ramanmandal2@gmail.com>
2020-03-05 21:30:01 +0530

    initial components

---
+++ employee/src/employees/Employees.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Employees extends React.Component<any, any>  {
+  public render() {
+    return (
+      <div>
+        Employees
+      </div>
+    );
+  }
+}
+
+export default Employees;
+  
---
+++ employee/src/employees/employee/Employee.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Employee extends React.Component<any, any>  {
+  public render() {
+    return (
+      <div>
+        Employee
+      </div>
+    );
+  }
+}
+
+export default Employee;
+  
