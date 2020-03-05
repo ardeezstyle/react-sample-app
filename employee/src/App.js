008e99e4fc89dee27b82d490a6aaba020aa3347d
Raman Mandal <ramanmandal2@gmail.com>
2020-03-05 21:58:54 +0530

    added semantic ui

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <Button primary> React App</Button>
     </div>
   );
 }
