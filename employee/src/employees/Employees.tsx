b701e5133eabe313a4d58def9e22e710731ded6e
Rupam Datta <rdatta@cloudera.com>
2020-03-08 18:11:15 +0530

    Created Service and used axios for making API calls

--- employee/src/employees/Employees.tsx
+++ employee/src/employees/Employees.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
-import {Table, Header} from 'semantic-ui-react';
+import {Table, Header, List} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import TodosService from '../services/todos-service';
+
 class Employees extends React.Component<any, any>  {
+
+  state = {
+    todos: [],
+    done: false
+  }
+
+  componentDidMount() {
+    const todoService = new TodosService();
+    todoService.getAllTodos().then((response: any) => this.setState({ todos: response.data, done: true }));
+
+    // todoService.getTodo(10).then((res: any) => console.log(res))
+  }
+
+  getTodoList = () => {
+    console.log(this.state.todos);
+    if(this.state && this.state.done) {
+        return <List>
+                {this.state.todos.map((todo: any) => <List.Item key={todo.id}>{todo.title} [{todo.userId}]</List.Item>)}
+              </List>
+    }
+    return null;
+  }
 
   renderLink = function(props) {
     let {text, link} = props;
@@ -41,6 +65,10 @@ class Employees extends React.Component<any, any>  {
             })}
           </Table.Body>
         </Table>
+
+        <hr />
+        {this.getTodoList()}
+
       </div>
     );
   }
